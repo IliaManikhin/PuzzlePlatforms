949f7bf932c8dda9a8ffb1969febeb9ada92c5e1
Ilia Manikhin <begbeder2012@yandex.ru>
2022-01-18 19:37:36 +0800

    Deactivate menu widget after we host to game

--- Source/PuzzlePlatforms/MainMenu.cpp
+++ Source/PuzzlePlatforms/MainMenu.cpp
@@ -22,6 +22,43 @@ void UMainMenu::SetMenuInterface(IMenuInterface* AMenuInterface)
 	this->MenuInterface = AMenuInterface;
 }
 
+void UMainMenu::Setup()
+{
+	this->AddToViewport();
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	APlayerController* PlayerController = World->GetFirstPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	FInputModeUIOnly InputModeData;
+
+	InputModeData.SetWidgetToFocus(this->TakeWidget());
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+
+	PlayerController->SetInputMode(InputModeData);
+	PlayerController->bShowMouseCursor = true;
+}
+
+void UMainMenu::Teardown()
+{
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	APlayerController* PlayerController = World->GetFirstPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->bShowMouseCursor = false;
+
+	FInputModeGameOnly InputModeGame;
+
+	PlayerController->SetInputMode(InputModeGame);
+
+	this->RemoveFromViewport();
+}
+
 void UMainMenu::HostServer()
 {
 	if (MenuInterface != nullptr)
